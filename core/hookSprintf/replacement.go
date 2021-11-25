98ecc7479031e8e9654888d07c43c24f3cbdbc9f
蒋吉兆 <303176530@qq.com>
2021-11-25 14:57:35 +0800

    hook strings.Join

--- core/hookSprintf/replacement.go
+++ core/hookSprintf/replacement.go
@@ -1,12 +1,12 @@
-·package hookSprintf
+package hookSprintf
 
 import "fmt"
 
 func Sprintf(format string, a ...interface{}) string {
 	fmt.Println("hook到方法fmt.Sprintf")
-	fmt.Println("入参",format,a)
-	s := SprintfT(format,a)
-	fmt.Println("回参",s)
+	fmt.Println("入参", format, a)
+	s := SprintfT(format, a)
+	fmt.Println("回参", s)
 	return s
 }
 
