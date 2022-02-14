e7b1b1afefa5e9f693dcef0b8961e6d6a4354693
piexlmax <303176530@qq.com>
2022-02-14 14:30:24 +0800

    Discarding configuration files and third-party packages to increase compatibility

--- global/config.go
+++ global/config.go
@@ -1,35 +1,44 @@
 package global
 
 import (
+	"flag"
 	"fmt"
 	"github.com/HXSecurity/DongTai-agent-go/model"
-	"github.com/spf13/pflag"
-	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
 )
 
 var Config model.Config
 
 func InitViper() {
-	v := viper.New()
-	v.SetConfigFile("dongtai-go-agent-config.yaml")
-	v.SetConfigType("yaml")
-	err := v.ReadInConfig()
+	config, err := ioutil.ReadFile("dongtai-go-agent-config.yaml")
+	fmt.Println(string(config))
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	pflag.String("DongtaiGoProjectVersion", "v1.0.0", "Project Version")
-	pflag.String("DongtaiGoProjectName", "GO Project", "Project Name")
-	pflag.Bool("DongtaiGoProjectCreate", true, "Auto Create Project")
-	pflag.Parse()
-	FErr := v.BindPFlags(pflag.CommandLine)
+	err = yaml.Unmarshal(config, &Config)
 	if err != nil {
-		fmt.Println(FErr)
+		fmt.Println(err)
 		return
 	}
-	configErr := v.Unmarshal(&Config)
-	if err != nil {
-		fmt.Println(configErr)
-		return
+
+	var version string
+	var name string
+	var auto bool
+
+	flag.StringVar(&version, "DongtaiGoProjectVersion", "v1.0.0", "Project Version")
+	flag.StringVar(&name, "DongtaiGoProjectName", "GO Project", "Project Name")
+	flag.BoolVar(&auto, "DongtaiGoProjectCreate", true, "Auto Create Project")
+	flag.Parse()
+	if version != "v1.0.0" {
+		Config.DongtaiGoProjectVersion = version
 	}
+	if name != "GO Project" {
+		Config.DongtaiGoProjectName = name
+	}
+	if auto != true {
+		Config.DongtaiGoProjectCreate = auto
+	}
+	fmt.Println(Config)
 }
